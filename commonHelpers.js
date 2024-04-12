7127a1c2f9f9c156cea8e17a8a445902a284d240
Christina-Mikheyeva <99538274+Christina-Mikheyeva@users.noreply.github.com>
2024-04-12 07:33:56 +0000

    Deploying to gh-pages from @ Christina-Mikheyeva/goit-js-hw-11@0aa328b50e92c4a953ce8c4cd685fc4836577754 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,22 +1,26 @@
-import{S as c,i as l}from"./assets/vendor-8c59ed88.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&t(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function t(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();const n=document.querySelector(".loader");function u(){n.classList.add("active")}function f(){n.classList.remove("active")}function d(s){const o=s.map(t=>`
+import{S as c,i as l}from"./assets/vendor-8c59ed88.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&t(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function t(e){if(e.ep)return;e.ep=!0;const r=n(e);fetch(e.href,r)}})();const a=document.querySelector(".loader");function u(){a.classList.add("active")}function d(){a.classList.remove("active")}function f(s){const o=s.map(t=>`
     <div class="gallery-item">
       <a href="${t.largeImageURL}" class="lightbox">
         <img src="${t.webformatURL}" alt="${t.tags}" />
       </a>
       <div class="info">
         <p>
-          <i class="fa fa-heart"></i> ${t.likes}
+        <b>Likes</b>
+         ${t.likes}
         </p>
         <p>
-          <i class="fa fa-eye"></i> ${t.views}
+        <b>Views</b>
+       ${t.views}
         </p>
         <p>
-          <i class="fa fa-comment"></i> ${t.comments}
+        <b>Comments</b>
+        ${t.comments}
         </p>
         <p>
-          <i class="fa fa-download"></i> ${t.downloads}
+        <b>Downloads</b>
+        ${t.downloads}
         </p>
       </div>
     </div>
-  `).join("");document.querySelector(".gallery").innerHTML=o,new c(".lightbox").refresh()}function p(s){l.show({title:"Пошук зображень",message:s,position:"topRight"})}async function h(s){const i=`https://pixabay.com/api/?key=43314388-88f7122255793401969da564c&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`;return(await(await fetch(i)).json()).hits}const m=document.querySelector(".search-form"),y=document.querySelector(".search-input");m.addEventListener("submit",async s=>{s.preventDefault();const o=y.value.trim();if(o==="")return;u();const i=await h(o);if(f(),i.length===0){p("Sorry, there are no images matching your search query. Please try again!");return}document.querySelector(".gallery").innerHTML="",d(i)});
+  `).join("");document.querySelector(".gallery").innerHTML=o,new c(".lightbox").refresh()}function p(s){l.show({title:"Пошук зображень",message:s,position:"topRight"})}async function m(s){const n=`https://pixabay.com/api/?key=43314388-88f7122255793401969da564c&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`;return(await(await fetch(n)).json()).hits}const h=document.querySelector(".search-form"),y=document.querySelector(".search-input");h.addEventListener("submit",async s=>{s.preventDefault();const o=y.value.trim();if(o==="")return;u();const n=await m(o);if(d(),n.length===0){p("Sorry, there are no images matching your search query. Please try again!");return}document.querySelector(".gallery").innerHTML="",f(n)});
 //# sourceMappingURL=commonHelpers.js.map
